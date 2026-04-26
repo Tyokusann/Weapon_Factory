51d1bba5e80fd70dc233c9ed076cf319deb152a9
Tyokusann <kuratyoku@gmail.com>
2026-04-26 13:50:44 +0900

    可愛いキャラクター表現と世界観演出を追加

--- src/scenes/BootScene.ts
+++ src/scenes/BootScene.ts
@@ -7,21 +7,44 @@ export class BootScene extends Phaser.Scene {
 
   create(): void {
     const playerGfx = this.add.graphics();
-    playerGfx.fillStyle(0x7bdff2, 1);
-    playerGfx.fillRect(0, 0, 24, 24);
+    playerGfx.fillStyle(0xffd7c2, 1);
+    playerGfx.fillRoundedRect(2, 4, 20, 18, 6);
+    playerGfx.fillStyle(0x6ac7ff, 1);
+    playerGfx.fillRoundedRect(2, 14, 20, 10, 4);
+    playerGfx.fillStyle(0x2b2d42, 1);
+    playerGfx.fillCircle(8, 11, 2);
+    playerGfx.fillCircle(16, 11, 2);
+    playerGfx.fillStyle(0xff8da1, 1);
+    playerGfx.fillCircle(6, 14, 1.5);
+    playerGfx.fillCircle(18, 14, 1.5);
     playerGfx.generateTexture('player', 24, 24);
     playerGfx.destroy();
 
     const slimeGfx = this.add.graphics();
-    slimeGfx.fillStyle(0x7ee081, 1);
-    slimeGfx.fillCircle(14, 14, 14);
+    slimeGfx.fillStyle(0x97f08a, 1);
+    slimeGfx.fillCircle(14, 15, 13);
+    slimeGfx.fillStyle(0xffffff, 1);
+    slimeGfx.fillCircle(10, 12, 2.5);
+    slimeGfx.fillCircle(18, 12, 2.5);
+    slimeGfx.fillStyle(0x293241, 1);
+    slimeGfx.fillCircle(10, 12, 1.2);
+    slimeGfx.fillCircle(18, 12, 1.2);
+    slimeGfx.fillStyle(0xff8fab, 1);
+    slimeGfx.fillEllipse(14, 18, 5, 2);
     slimeGfx.generateTexture('slime', 28, 28);
     slimeGfx.destroy();
 
     const nodeGfx = this.add.graphics();
-    nodeGfx.fillStyle(0xa86f33, 1);
-    nodeGfx.fillCircle(10, 10, 10);
-    nodeGfx.generateTexture('woodNode', 20, 20);
+    nodeGfx.fillStyle(0x81512f, 1);
+    nodeGfx.fillCircle(10, 12, 9);
+    nodeGfx.fillStyle(0x9c6b3d, 1);
+    nodeGfx.fillCircle(10, 12, 6.5);
+    nodeGfx.fillStyle(0x5f3a20, 1);
+    nodeGfx.fillCircle(10, 12, 2.5);
+    nodeGfx.fillStyle(0x4cbf6b, 1);
+    nodeGfx.fillCircle(6, 5, 2.5);
+    nodeGfx.fillCircle(13, 4, 2.5);
+    nodeGfx.generateTexture('woodNode', 20, 22);
     nodeGfx.destroy();
 
     this.scene.start('TitleScene');
--- src/scenes/TitleScene.ts
+++ src/scenes/TitleScene.ts
@@ -8,17 +8,22 @@ export class TitleScene extends Phaser.Scene {
   create(): void {
     this.cameras.main.setBackgroundColor('#20334f');
 
-    this.add.text(400, 190, 'Weapon Factory', {
+    this.add.text(400, 170, 'Weapon Factory', {
       fontSize: '48px',
       color: '#f6f8ff'
     }).setOrigin(0.5);
 
-    this.add.text(400, 260, '森で素材を集めて木の剣を作ろう', {
+    this.add.text(400, 235, 'かわいい冒険者で森を探索しよう！', {
+      fontSize: '24px',
+      color: '#ffd6e0'
+    }).setOrigin(0.5);
+
+    this.add.text(400, 280, '素材を集めて木の剣をクラフト', {
       fontSize: '22px',
       color: '#dce7ff'
     }).setOrigin(0.5);
 
-    this.add.text(400, 340, 'Enter / クリックで開始', {
+    this.add.text(400, 350, 'Enter / クリックで開始', {
       fontSize: '24px',
       color: '#ffe3a5'
     }).setOrigin(0.5);
